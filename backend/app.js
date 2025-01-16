569d0e51ad5a8116d3ca17fae1a520bf5a4f8b69
jacobota <otajacob@yahoo.com>
2025-01-15 20:53:41 -0800

    Added TODO routers and a middleware to log api calls

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,24 @@
 const express = require("express");
 const { logger } = require("./src/util/logger");
 
-//create the server on PORT 3000
+// Routers (TODO)
+
+// Create the server on PORT 3000
 const app = express();
 const PORT = 3000;
 
-//initialize server
 app.listen(PORT, () => {
     logger.info(`Started the server on Port ${PORT}`);
 });
 
+// Middleware that logs any incoming requests
+app.use((req, res, next) => {
+    logger.info(`${req.method} ${req.url}`);
+    next();
+});
+
+// HTTP Routes (TODO)
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
