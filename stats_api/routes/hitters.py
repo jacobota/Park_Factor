2cddd5150e13b4e58cf782d63d751d0b9a8ed4e5
jacobota <otajacob@yahoo.com>
2025-02-01 22:39:34 -0800

    Switched from checking player names to player ids

--- stats_api/routes/hitters.py
+++ stats_api/routes/hitters.py
@@ -8,32 +8,33 @@ hitter_route = Blueprint('hitter_route', __name__)
 def home():
     return jsonify( {'message': 'Hitter API'} )
 
-@hitter_route.route('/api/hitter-stats')
+@hitter_route.route('/api/hitter-stats/current-season')
 def get_hitter_stats_this_season():
     try:
-        # Get hitter first and last name from query parameters
-        full_name = request.args.get('name')
-        if full_name:
-            full_name = full_name.replace('_', ' ').lower()
+        # Get hitter id from query parameters
+        key_fangraphs = request.args.get('fg-id')
+
+        # Check if key_fangraphs is given or convert to int
+        if not key_fangraphs:
+            return jsonify({'error': 'Fangraph ID required'}), 400
         else:
-            return jsonify({'error': 'Name is required'}), 400
+            key_fangraphs = int(key_fangraphs)
         
-        # Get Fangraphs batting stats for the current season and filter by player name
-        fg_data = pb.batting_stats(2024, qual=1);
-        fg_data['Name'] = fg_data['Name'].str.lower()
-        fg_record = fg_data[fg_data['Name'] == full_name].to_dict('records')
+        # Get Fangraphs batting stats for the current season and filter by playerid
+        fg_hitter_data = pb.batting_stats(2024, qual=1);
+        fg_hitter_record = fg_hitter_data[fg_hitter_data['IDfg'] == key_fangraphs].to_dict('records')
         
-        if not fg_record:
-            return jsonify({'full_name': full_name, 'player_stats': None})
+        if not fg_hitter_record:
+            return jsonify({'playerid': key_fangraphs, 'hitter_stats': None})
         
         # Select specific attributes to return
         selected_attributes = ['Name', 'G', 'AVG', 'OBP', 'SLG', 'OPS', 'HR', 'R', 'H', 'RBI', 'SB', 'wOBA', 'xwOBA', 'xBA', 'xSLG', 'EV', 'maxEV', 'Barrel%', 'HardHit%', 'Swing%', 'Z-Swing%', 'Contact%', 'WPA', 'BB%', 'K%', 'BB/K', 'BsR', 'SB% (pi)', 'wSB', 'ISO', 'BABIP']
         fg_selected_attribute_record = [
-            {attr: player[attr] for attr in selected_attributes if attr in player}
-            for player in fg_record
+            {attr: hitter[attr] for attr in selected_attributes if attr in hitter}
+            for hitter in fg_hitter_record
         ]
         
-        return jsonify({'player_stats': fg_selected_attribute_record})
+        return jsonify({'player_id': key_fangraphs, 'hitter_stats': fg_selected_attribute_record})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
