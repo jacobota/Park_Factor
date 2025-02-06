491a9c0d3714d693843a56e0d274879e5bc1f669
jacobota <otajacob@yahoo.com>
2025-02-05 21:41:08 -0800

    Added Pitcher biographical information

--- stats_api/routes/players.py
+++ stats_api/routes/players.py
@@ -85,7 +85,7 @@ def test():
         # Player biographical info based on bbref id
         people_df = lahman.people()
         people_record = people_df[people_df['bbrefID'] == bbref_id].to_dict('records')
-        people_record_selected_attributes = ['birthCountry',  'weight', 'height', 'bats', 'throws']
+        people_record_selected_attributes = ['birthYear', 'birthCountry',  'weight', 'height', 'bats', 'throws']
         people_selected_attribute_record = [
             {attr: hitter[attr] for attr in people_record_selected_attributes if attr in hitter}
             for hitter in people_record
@@ -95,19 +95,48 @@ def test():
         statcast_hitter_bio_data = pb.statcast_sprint_speed(current_year)  
         statcast_hitter_bio_record = statcast_hitter_bio_data[statcast_hitter_bio_data['player_id'] == mlbam_id].to_dict('records')      
 
-        statcast_hitter_bio_selected_attributes = ['last_name, first_name', 'age', 'team', 'position', 'team_id']
+        statcast_hitter_bio_selected_attributes = ['last_name, first_name', 'age', 'team', 'position']
         statcast_hitter_bio_record = [
             {attr: sprint_speed[attr] for attr in statcast_hitter_bio_selected_attributes if attr in sprint_speed}
             for sprint_speed in statcast_hitter_bio_record
         ]
 
-        # TODO: Pitcher Biographic Information
+        # Get Pitcher Pitch Types and Average speeds
+        statcast_pitcher_speed_data = pb.statcast_pitcher_pitch_arsenal(current_year, minP=1)
+        statcast_pitcher_speed_record = statcast_pitcher_speed_data[statcast_pitcher_speed_data['pitcher'] == mlbam_id].to_dict('records')
+        
+        statcast_pitcher_speed_selected_attributes = ['ch_avg_speed', 'cu_avg_speed', 'fc_avg_speed', 'ff_avg_speed', 'fs_avg_speed', 'kn_avg_speed', 'si_avg_speed', 'sl_avg_speed', 'st_avg_speed', 'sv_avg_speed']
+        statcast_pitcher_speed_record = [
+            {attr: pitch_speed[attr] for attr in statcast_pitcher_speed_selected_attributes if attr in pitch_speed}
+            for pitch_speed in statcast_pitcher_speed_record
+        ]
+
+        statcast_pitcher_bio_data = pb.statcast_pitcher_arsenal_stats(current_year, minPA=1)
+        statcast_pitcher_bio_record = statcast_pitcher_bio_data[statcast_pitcher_bio_data['player_id'] == mlbam_id].to_dict('records')
+
+        statcast_pitcher_bio_selected_attributes = ['last_name, first_name', 'team_name_alt']
+        statcast_pitcher_bio_record = [
+            {attr: pitch_arsenal[attr] for attr in statcast_pitcher_bio_selected_attributes if attr in pitch_arsenal}
+            for pitch_arsenal in statcast_pitcher_bio_record
+        ]
+
+        # Add position for pitcher (LHP or RHP)
+        if statcast_pitcher_bio_record:
+            throws = people_selected_attribute_record[0].get('throws')
+            if throws == 'L':
+                statcast_pitcher_bio_record[0]['pitcher_position'] = 'LHP'
+            elif throws == 'R':
+                statcast_pitcher_bio_record[0]['pitcher_position'] = 'RHP'
 
         player_bio = {}
         if people_selected_attribute_record:
             player_bio.update(people_selected_attribute_record[0])
         if statcast_hitter_bio_record:
             player_bio.update(statcast_hitter_bio_record[0])
+        if statcast_pitcher_speed_record:
+            player_bio.update(statcast_pitcher_speed_record[0])
+        if statcast_pitcher_bio_record:
+            player_bio.update(statcast_pitcher_bio_record[0])
 
         # Player award info based on bbref id
         awards_df = lahman.awards_players()
