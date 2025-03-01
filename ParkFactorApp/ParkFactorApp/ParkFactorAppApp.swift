fd403f5fe9e87af647fcd36b2d909acd7690f39c
jacobota <otajacob@yahoo.com>
2025-02-28 22:58:02 -0800

    Hello World for ParkFactor App

---
+++ ParkFactorApp/ParkFactorApp/ParkFactorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkFactorAppApp.swift
+//  ParkFactorApp
+//
+//  Created by Jacob Ota on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkFactorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkFactorApp/ParkFactorAppTests/ParkFactorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkFactorAppTests.swift
+//  ParkFactorAppTests
+//
+//  Created by Jacob Ota on 2/28/25.
+//
+
+import Testing
+@testable import ParkFactorApp
+
+struct ParkFactorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
