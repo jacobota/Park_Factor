ccdcc51f9691ae1c18ec3ba462f36e8a94ac16b3
jacobota <otajacob@yahoo.com>
2025-03-08 12:20:08 -0800

    Added logic to check if user is indeed a user by access token during the loading screen, and updates the savedUser if not already fully updated

--- ParkFactorApp/ParkFactorApp/ContentView.swift
+++ ParkFactorApp/ParkFactorApp/ContentView.swift
@@ -34,11 +34,87 @@ struct ContentView: View {
     
     private func checkLoginStatus() {
         // Checks if the value in accessToken is not nil with a delay
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            if let _ = accessToken {
-                isLoggedIn = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            // check if there is a token available to use
+            guard accessToken != nil else {
+                DispatchQueue.main.async {
+                    isLoggedIn = false
+                    isLoading = false
+                }
+                return
             }
-            isLoading = false
+            
+            // call the network request to retrieve user
+            let baseUrl = Env.expressBaseURL
+            guard let url = URL(string: "\(baseUrl)/users/profile") else {
+                // Consider fatalError here if server is not active
+                DispatchQueue.main.async {
+                    isLoggedIn = false
+                    isLoading = false
+                }
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("Bearer \(accessToken!)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = "GET"
+            
+            let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    DispatchQueue.main.async {
+                        accessToken = nil
+                        isLoggedIn = false
+                        isLoading = false
+                    }
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse else {
+                    DispatchQueue.main.async {
+                        accessToken = nil
+                        isLoggedIn = false
+                        isLoading = false
+                    }
+                    return
+                }
+                
+                if response.statusCode != 200 {
+                    DispatchQueue.main.async {
+                        accessToken = nil
+                        isLoggedIn = false
+                        isLoading = false
+                    }
+                    return
+                }
+                
+                guard let data = data else {
+                    DispatchQueue.main.async {
+                        accessToken = nil
+                        isLoggedIn = false
+                        isLoading = false
+                    }
+                    return
+                }
+                
+                do {
+                    let decodedUser = try JSONDecoder().decode(User.self, from: data)
+                    DispatchQueue.main.async {
+                        savedUser.user = decodedUser
+                        isLoggedIn = true
+                        isLoading = false
+                    }
+                } catch _ {
+                    DispatchQueue.main.async {
+                        accessToken = nil
+                        isLoggedIn = false
+                        isLoading = false
+                    }
+                    return
+                }
+            }
+            
+            dataTask.resume()
         }
     }
 }
